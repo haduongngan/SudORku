81680973aebfcdcf905f8fce78fbadb6911bef4a
hatnho0708 <19020061@vnu.edu.vn>
2020-07-22 17:38:40 +0700

    initial commit

---
+++ cmake-build-debug/CMakeFiles/sudoku.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sudoku.dir/main.cpp.o"
+  "sudoku"
+  "sudoku.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sudoku.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
